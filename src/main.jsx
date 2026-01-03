69e13e4c10c0d6d0945e49d25d65128ec73b5cd1
Nitish Soni <154347189+soni100ni@users.noreply.github.com>
2026-01-03 19:21:51 +0530

    Rename Routes import to AppRoutes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router";
-import Routes from "./config/routes.jsx";
+import AppRoutes from "./config/routes.jsx";
 import { Toaster } from "react-hot-toast";
 import { ChatProvider } from "./context/ChatContext.jsx";
 
