77e33b8b37d12bbf4d4d82cec4c36759fa49c7ec
Nitish Soni <154347189+soni100ni@users.noreply.github.com>
2026-01-04 14:53:31 +0530

    Change baseURL to production endpoint

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseURL = "http://localhost:8080";
+export const baseURL = "https://chat-backend.onrender.com";
 export const httpClient = axios.create({
   baseURL: baseURL,
 });
